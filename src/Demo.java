e6542f4ddcd2b74701ab54dc0cee9296835b91c8
Roshinidevops22 <roshinishithanush1@gmail.com>
2024-02-14 18:27:19 +0530

    Intial Commit

---
+++ src/Demo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please enter age:");
+        int age= scan.nextInt();
+        System.out.println("Age is:" +age);
+    }
+}
